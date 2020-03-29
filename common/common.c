fd6ace5e58b48981a54e463c4302af7a1751dec3
431009479 <431009479@qq.com>
2020-03-29 11:54:36 +0800

    V0.1-0.2

--- common/common.c
+++ common/common.c
@@ -4,25 +4,43 @@
 	> Mail: suyelu@haizeix.com
 	> Created Time: Sat 28 Mar 2020 08:41:00 PM CST
  ************************************************************************/
+ #include"head.h"
 
-#include "head.h"
 
 void make_nonblock_ioctl(int fd){
     unsigned long ul = 1;
-    ioctl(fd, FIONBIO, &ul);
+    ioctl(fd, FIONBIO,&ul);
 }
-
-
-void make_block_ioctl(int fd) {
+void make_block_ioctl(int fd){
     unsigned long ul = 0;
     ioctl(fd, FIONBIO, &ul);
 }
 
 
-void make_nonblock(int fd) {
-    fcntl(fd, F_SETFL, O_NONBLOCK);
+void make_nonblock(int fd){
+    int flags;
+    if(flags = fcntl(fd, F_GETFL, 0) < 0){
+        perror("fcntl");
+        return ;
+    }
+    flags |= O_NONBLOCK;
+    if(fcntl(fd, F_SETFL, flags) < 0){
+        perror("fcntl");
+        return ;
+    }
+
 }
 
-void make_block(int fd) {
-    fcntl(fd, F_SETFL, ~O_NONBLOCK);
+void make_block(int fd){
+    int flags;
+    if(flags = fcntl(fd, F_GETFL, 0) < 0){
+        perror("fcntl");
+        return ;
+    }
+    flags &= ~O_NONBLOCK;
+    if(fcntl(fd, F_SETFL, flags) < 0){
+        perror("fcntl");
+        return ;
+    }
 }
+
